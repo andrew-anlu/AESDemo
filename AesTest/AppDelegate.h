4fa358d66f9a127397087db472392bae559791f1
wecl <WECL@nnit.com>
2016-07-01 15:23:21 +0800

    Initial Commit

---
+++ AesTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AesTest
+//
+//  Created by NNIT on 7/1/16.
+//  Copyright © 2016 NNIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AesTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AesTest
+//
+//  Created by NNIT on 7/1/16.
+//  Copyright © 2016 NNIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
